379f61e02dee578f5e51043a92c181a0eaa0c293
cauindodev <167608133+cauindodev@users.noreply.github.com>
2024-04-22 10:32:52 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'S';
+const giftUrl = '#';
+const eventName = 'Chúc Mừng Sinh Nhựt Em Bé sí';
+const titleCard = 'Em bé sí thưm';
+const contentCard = 'Chúc em bé của tui sinh nhựt thiệt vui vẻ xinh đẹp nun nun iu thưn tui hứm hứm';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'si_thum.jpeg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
